952e0df61f05ba5033b687760f0be8f06b680309
rizhul.januar <rizhul.januar@kopnus.com>
2026-01-13 18:40:39 +0700

    feat: penambahan interceptor dan type safe

--- my-cv/src/users/users.controller.ts
+++ my-cv/src/users/users.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Post, Get, Patch, Delete, Body, Param, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from './user.entity';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDTO } from './dtos/user.dto';
+import { Serialize } from 'src/seriallize/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -10,8 +11,8 @@ export class UsersController {
 
   // CREATE: Buat user baru
   @Post('signup')
+  @Serialize(UserDTO)
   createUser(@Body() createUserDto: CreateUserDto) {
-    console.log(createUserDto)
     return this.usersService.create(
       createUserDto.email,
       createUserDto.password
@@ -20,24 +21,28 @@ export class UsersController {
 
   // FIND ONE: Cari user by ID
   @Get(':id')
-  findOneUser(@Param('id') id: string) {
+  @Serialize(UserDTO)
+  findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
   }
 
   // FIND: Cari users by email
   @Get()
+  @Serialize(UserDTO)
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
   // UPDATE: Update user
   @Patch(':id')
+  @Serialize(UserDTO)
   updateUser(@Param('id') id: string, @Body() attrs: UpdateUserDto) {
     return this.usersService.update(parseInt(id), attrs);
   }
 
   // DELETE: Hapus user
   @Delete(':id')
+  @Serialize(UserDTO)
   removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
